61e4fdf10904bc65f3e5417b4943e2d2f1fe38ab
Joel Azemar <joel.azemar@gmail.com>
2023-01-06 17:04:41 +0100

    Update application_system_test_case.rb

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,9 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  if ENV["CI"].present?
+    driven_by :selenium, using: :headless_chrome
+  else
+    driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  end
 end
