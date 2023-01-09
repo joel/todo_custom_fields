8373b2b26d0f33ef90df7004eb8646f8aa599fcb
Joel Azemar <joel.azemar@gmail.com>
2023-01-09 12:58:30 +0100

    Add Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+{
+  "Grocery" => [
+    "Buy milk",
+    "Buy eggs",
+  ],
+  "Home" => [
+    "Clean the house",
+    "Wash the dishes",
+  ],
+  "Personal" => [
+    "Go to the gym",
+    "Go to the doctor",
+  ],
+  "Errands" => [
+    "Go to the bank",
+  ],
+  "Shopping" => [
+    "Buy a new phone",
+    "Buy a new laptop",
+  ],
+}.each do |todo_name, items|
+  todo_list = Todo.create(name: todo_name)
+
+  items.each do |item_name|
+    todo_list.items.create(name: item_name)
+  end
+end
