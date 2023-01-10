d135f18f32e1e383b4eff70c77eaa5dec40a66c2
Joel Azemar <joel.azemar@gmail.com>
2023-01-10 13:08:06 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_109_113_312) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_09_113312) do
   create_table "fields", force: :cascade do |t|
     t.string "name"
     t.json "metadata", default: {}
@@ -23,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 20_230_109_113_312) do
     t.string "field_type", default: "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index %w[source_type source_id], name: "index_fields_on_source"
+    t.index ["source_type", "source_id"], name: "index_fields_on_source"
   end
 
   create_table "items", force: :cascade do |t|
