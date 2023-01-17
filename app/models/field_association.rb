e1fecc34e531cc7699c672583c7941908a8bb86e
Joel Azemar <joel.azemar@gmail.com>
2023-01-17 12:50:56 +0100

    Update field_association.rb

--- app/models/field_association.rb
+++ app/models/field_association.rb
@@ -16,6 +16,7 @@ class FieldAssociation < ApplicationRecord
   def set_value_type
     return unless field_changed?
 
+    # This can't work as it change the definition of the class, we need to create a Singleton class, which seems really hard to do.
     mod = "::Attributes::#{field.field_type.capitalize}".constantize
     self.class.include(mod) unless self.class.included_modules.include?(mod)
   rescue NameError
